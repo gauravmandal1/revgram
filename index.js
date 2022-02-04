3e81a724d93f4bf866c87602023cbdb0a7f7d2a7
gauravmandal1 <64638825+gauravmandal1@users.noreply.github.com>
2022-02-04 19:19:35 +0530

    ok

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoutes);
 app.get('/',(req, res)=>{
   res.send('Hello to Memories Api')
 })
+app.use('/posts', postRoutes);
+
 app.use("/user", userRouter);
 
 const CONNECTION_URL = "mongodb+srv://abcd:abcd@cluster0.727i3.mongodb.net/MERN?retryWrites=true&w=majority";
