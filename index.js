3bd92a99d1bd56e45bc5b1633d532f77b5c3b700
gauravmandal1 <64638825+gauravmandal1@users.noreply.github.com>
2022-03-22 14:58:17 +0530

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
  
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
@@ -13,16 +16,16 @@ app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.get('/',(req, res)=>{
-  res.send('Hello to Memories Api')
+  res.send('Hello to Revgram Api')
 })
 app.use('/posts', postRoutes);
 
 app.use("/user", userRouter);
 
-const CONNECTION_URL = "mongodb+srv://abcd:abcd@cluster0.727i3.mongodb.net/MERN?retryWrites=true&w=majority";
+// const CONNECTION_URL = "mongodb+srv://abcd:abcd@cluster0.727i3.mongodb.net/MERN?retryWrites=true&w=majority";
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
